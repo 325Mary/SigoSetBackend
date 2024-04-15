2aeef1a0a3f3db283888f22c52391dcd1a034bad
325Mary <mcordoba577@misena.edu.co>
2024-04-15 12:37:00 -0500

    CRUD basico de perfil

--- src/config/server.js
+++ src/config/server.js
@@ -1,9 +1,8 @@
 // Importación de módulos y configuraciones necesarios para el servidor
 const express = require("express");
-const morgan = require('morgan');
 const usuarioRoutes = require('../routes/usuario.routes')
-const centroFormacionRoutes = require('../routes/centroFormacion.routes')
-const zonaRoutes = require('../routes/zona.routes')
+const perfilRoutes = require('../routes/perilRoutes')
+
 // Configuración del servidor Express
 const appSigoSet = express();
 const port = 3000;
@@ -11,9 +10,8 @@ const port = 3000;
 appSigoSet.use(express.json());
 
 appSigoSet.use(usuarioRoutes);
-appSigoSet.use(centroFormacionRoutes)
-appSigoSet.use(zonaRoutes)
-appSigoSet.use(morgan("dev"));
+appSigoSet.use(perfilRoutes);
+
 appSigoSet.set("port", process.env.PORT || port);
 
 module.exports = appSigoSet;
