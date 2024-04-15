d359dba74ad69f64fe9a4fa2201a00920f9c6998
325Mary <mcordoba577@misena.edu.co>
2024-04-14 22:55:09 -0500

    CRUD basico de empresas, pendiente por modificar el id

--- src/config/server.js
+++ src/config/server.js
@@ -1,6 +1,7 @@
 // Importación de módulos y configuraciones necesarios para el servidor
 const express = require("express");
 const usuarioRoutes = require('../routes/usuario.routes')
+const empresaRoutes= require('../routes/empresaRoutes')
 // Configuración del servidor Express
 const appSigoSet = express();
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 appSigoSet.use(express.json());
 
 appSigoSet.use(usuarioRoutes)
+appSigoSet.use(empresaRoutes)
 
 appSigoSet.set("port", process.env.PORT || port);
 
